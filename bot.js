f5fea7e4668a9564ad0104c2619438b7158ae1c9
SiriusAvaldi <51836853+SiriusAvaldi@users.noreply.github.com>
2019-06-15 17:33:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,8 +35,39 @@ var prefix = '!'
 			}
 		})
      }
+     if(message.content.startsWith(prefix + 'Ивенты')) {
+		request('https://api.warframestat.us/pc/events', function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var info = JSON.parse(body);
+				var index;
+				var mes = "";
+				if(info.length > 0)
+				{
+					for (index = 0; index < info.length; index++) {
+					    mes += info[index].description + ' - ' + info[index].tooltip + ' - ' + info[index].expiry + '\r\n';
+					}
+					message.channel.sendMessage(mes);
+				}
+			}
+		})
+     }
+     if(message.content.startsWith(prefix + 'Вылазка')) {
+		request('https://api.warframestat.us/pc/sortie', function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var info = JSON.parse(body);
+				var index;
+				var mes = "";
+				mes += info.faction + ' - ' + info.eta + '\r\n';
+				for (index = 0; index < info.variants.length; index++) {
+				    mes += info.variants[index].missionType + ' - ' + info.variants[index].node + ' - ' + info.variants[index].modifier + '\r\n';
+				}
+				message.channel.sendMessage(mes);
+			}
+		})
+     }
      if(message.content.startsWith(prefix + 'help')) {
-	var mes = "!help - Список команд\r\n!Цетус - Информация о времени стуток на Равнинай Эйдолона\r\n!Разломы - Информация о разломах бездны\r\n";
+	var mes = "!help - Список команд\r\n!Цетус - Информация о времени стуток на Равнинай Эйдолона\r\n!Разломы - Информация о разломах бездны\r\n!Ивенты - Информация об активных ивентах\r\n!Вылазка - Информация о текущей вылазке\r\n";
+     	message.channel.sendMessage(mes);
      }
 	 
  });
