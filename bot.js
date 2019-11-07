0e75cc973e2d4ff2d2cd34e626d2840eb129bcc1
SiriusAvaldi <51836853+SiriusAvaldi@users.noreply.github.com>
2019-11-07 18:25:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,17 +5,17 @@ const client = new Discord.Client();
 
 var prefix = '!'
 client.on("ready", () => {
-	var date1 = new Date('12-11-2019');
+	var date1 = new Date('11-12-2019');
 	var date2 = new Date();
 	var daysLag = Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / (1000 * 3600 * 24));
-	client.user.setActivity(`${daysLag} дней`);
+	client.user.setActivity(`ожидание ${daysLag} дней `);
  });
  
  client.on('message', message => {
-    var date1 = new Date('12-11-2019');
+    var date1 = new Date('11-12-2019');
     var date2 = new Date();
     var daysLag = Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / (1000 * 3600 * 24));
-    client.user.setActivity(`${daysLag} дней`);
+    client.user.setActivity(`ожидание ${daysLag} дней`);
 	 
     if(message.author === client.user) return;
     if(message.content.startsWith(prefix + 'Цетус')) {
