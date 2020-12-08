31d77e259d2163637609d3d01c1b77012da7512a
SiriusAvaldi <51836853+SiriusAvaldi@users.noreply.github.com>
2020-12-08 14:13:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 var prefix = '!'
 client.on("ready", () => {
 	var date1 = new Date();
-	var date2 = new Date('12-05-2019');
+	var date2 = new Date('11-24-2020');
 	var daysLag = Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / (1000 * 3600 * 24));
 	client.user.setActivity(`армии уже ${daysLag} дней`);
  });
