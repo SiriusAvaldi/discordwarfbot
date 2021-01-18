ab0e7451cf9340dedd235cb383e864e2a002b1c9
SiriusAvaldi <51836853+SiriusAvaldi@users.noreply.github.com>
2021-01-18 18:53:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -160,12 +160,10 @@ client.on("ready", () => {
 		})
      }
      if(message.content.startsWith(prefix + 'дембель')) {
-	var temp = message.content();
-     	var temp = temp.toLowerCase();
 	var date1 = new Date('11-24-2021');
         var date2 = new Date();
         var daysLag = Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / (1000 * 3600 * 24));
-	message.channel.sendMessage('Осталось ' + daysLag + ' дней ' + temp);
+	message.channel.sendMessage('Осталось ' + daysLag + ' дней');
      }
      if(message.content.startsWith(prefix + 'help')) {
 	var mes = prefix + "help - Список команд\r\n"
